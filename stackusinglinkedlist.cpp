03f4a647b89a4612211a0a0bf6f84ff495f7f8bf
ilyaspasha1 <muhammad.ilyas.ft24@mail.umy.ac.id>
2025-05-20 11:26:02 +0700

    add while loop menu option

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -1,2 +1,88 @@
-cout << "Stack is Empty. Cannot pop." << endl;
-cout << "Stack is Empty. No top value" << endl;
+#include <iostream>
+using namespace std;
+
+
+
+
+class Node
+{
+    public:
+        int data;
+        Node *next;
+
+        Node()
+        {
+                next = NULL;
+        }
+};
+
+class stack
+{
+    private:
+        Node *top;
+
+    public:
+        stack()
+        {
+            top = NULL;
+        }
+
+        int push(int value)
+        {
+            Node *newNode = new Node();
+            newNode->data = value;
+            newNode->next = top;
+            top = newNode;
+            cout << "Push Value: " << value << endl;
+            return value;
+
+        }
+
+        void pop()
+        {
+            if (isEmpty())
+            {
+                cout << "Stack is Empty" << endl;
+            }
+
+            cout << "Popped Value: " << top->data << endl;
+            top = top->next;
+        }
+
+        void peek()
+        {
+            if (top == NULL)
+            {
+                cout << "List is Empty." << endl;
+            }
+            else{
+                    Node *current = top;
+                    while (current != NULL)
+                    {
+                        cout << current->data << " " << endl;
+                        current = current->next;
+                    }
+                    cout << endl;
+            }
+        }
+    bool isEmpty() {
+        return top = NULL;
+    }
+};
+
+int main() {
+    stack stact;
+
+    int choice = 0;
+    int value;
+
+    while (choice != 5) {
+        cout << "1. Push\n";
+        cout << "2.Pop\n";
+        cout << "3.Peek\n";
+        cout << "4.exit\n";
+        cout << "Enter your choice : ";
+        cin >> choice;
+
+        switch (choice) {
+           
