5a7540f854775cfe8a7ab76c30a926543dbcfdc6
ilyaspasha1 <muhammad.ilyas.ft24@mail.umy.ac.id>
2025-05-20 11:45:49 +0700

    handle peek option in switch

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -98,4 +98,12 @@ int main() {
                     cout << "Stact is Empty. Cannot pop." << endl;
                 }
                 break;
-           
+            case 3:
+                if (!stact.isEmpty()) {
+                    stact.peek();
+                }
+                else {
+                    cout << "Stact is Empty. No top value" << endl;
+                }
+                break;
+            
