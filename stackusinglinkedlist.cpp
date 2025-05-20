4aba75336957e96926d9addd1d43ef4fae35234a
ilyaspasha1 <muhammad.ilyas.ft24@mail.umy.ac.id>
2025-05-20 11:27:02 +0700

    handle push option in switch

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -85,4 +85,9 @@ int main() {
         cin >> choice;
 
         switch (choice) {
-           
+            case 1:
+                cout << "Enter the value to push : ";
+                cin >> value;
+                stact.push(value);
+                break;
+            
