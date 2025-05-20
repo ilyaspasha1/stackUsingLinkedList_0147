254043554272dfd855183fa68c0b4bc938eb5597
ilyaspasha1 <muhammad.ilyas.ft24@mail.umy.ac.id>
2025-05-20 11:00:28 +0700

    impliment push() function for stack

--- stackusinglinkedlist.cpp
+++ stackusinglinkedlist.cpp
@@ -1,7 +1,38 @@
-class Node {
-public:
-    int data;
-    Node* next;
+#include <iostream>
+using namespace std;
 
-    Node(int value) : data(value), next(nullptr) {}
+
+
+class Node
+{
+    public:
+        int data;
+        Node *next;
+
+        Node()
+        {
+                next = NULL;
+        }
 };
+
+class stack
+{
+    private:
+        Node *top;
+
+    public:
+        stack()
+        {
+            top = NULL;
+        }
+
+        int push(int value)
+        {
+            Node *newNode = new Node();
+            newNode->data = value;
+            newNode->next = top;
+            top = newNode;
+            cout << "Push Value: " << value << endl;
+            return value;
+
+        }
